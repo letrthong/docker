385c4572db572bbc09f07dc5490c3bb10b6218f0
Thong LT <letrthong@gmail.com>
2025-05-07 11:42:30 +0700

    Update upload_download_file.py

--- ubuntu-ssh/upload_download_file.py
+++ ubuntu-ssh/upload_download_file.py
@@ -8,6 +8,8 @@ def create_ssh_client(server, port, user, password):
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(server, port, user, password)
+    if client == None:
+        print("Can not access to server: %s")
     return client
 
 def transfer_file(ssh_client, local_file, remote_path):
@@ -22,7 +24,7 @@ def download_file(ssh_client, remote_file, local_path):
 
 # Example usage
 server = 'localhost'
-server = '10.0.2.15'
+#server = '10.0.2.15'
 port = 2222
 user = 'root'
 password = 'telua'
@@ -32,6 +34,6 @@ remote_path = '/home'
 ssh_client = create_ssh_client(server, port, user, password)
 transfer_file(ssh_client, local_file, remote_path)
 
-remote_file = '/home/test.py'
+remote_file = remote_path + "/" + local_file
 local_path = 'download.py'
 download_file(ssh_client, remote_file, local_path)
