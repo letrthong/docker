967bdc3f26d28421caaa960a2634f30aeee6bbcc
Thong LT <letrthong@gmail.com>
2025-06-17 09:33:59 +0700

    Update and rename ssh_config to main.cpp

---
+++ ubuntu-gcc/main.cpp
@@ -0,0 +1,14 @@
+
+/*
+ FileName: main.cpp
+ Docker https://hub.docker.com/_/gcc/
+*/
+
+
+#include <iostream> // Include the input-output stream library
+
+int main() {
+   
+    std::cout << "Hello, World! on docker with gcc:4.9" << std::endl; 
+    return 0; // Indicate that the program ended successfully
+}
