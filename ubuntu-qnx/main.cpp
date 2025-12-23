4f549ff7d99c991e3b8e1848ba304afd536dcc5e
Your Name <you@example.com>
2025-12-23 21:15:11 +0700

    qnx

---
+++ ubuntu-qnx/main.cpp
@@ -0,0 +1,15 @@
+
+/*
+ FileName: main.cpp
+ Docker https://hub.docker.com/_/gcc/
+*/
+
+
+#include <iostream> // Include the input-output stream library
+
+int main() {
+   
+    std::cout << "Hello, World! on docker with gcc:4.9" << std::endl;
+    return 1; // Indicate that the program ended successfully
+}
+
