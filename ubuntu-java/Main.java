b862e725da049f9ba7e31acc2a1a262cbcd88927
Thong LT <letrthong@gmail.com>
2026-01-30 21:26:31 +0700

    x

---
+++ ubuntu-java/Main.java
@@ -0,0 +1,10 @@
+/*
+ FileName: Main.java
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        // In Java, System.out.println tự động xuống dòng (tương đương std::endl)
+        System.out.println("Hello, World! on docker with Java");
+    }
+}
