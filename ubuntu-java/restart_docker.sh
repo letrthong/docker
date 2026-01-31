ca030eac795106c72a9075ed590ca3c375938ca9
Thong LT <letrthong@gmail.com>
2026-01-31 14:01:27 +0700

    x

--- ubuntu-java/restart_docker.sh
+++ ubuntu-java/restart_docker.sh
@@ -1,6 +1,8 @@
 # remove the rest containers if have
 docker compose down
 
+# Đảm bảo xóa container cũ nếu lệnh down chưa xử lý hết hoặc container được tạo thủ công
+docker rm -f ubuntu_java 2>/dev/null || true
  
 
 # start up containers from all images
--- ubuntu-java/start_docker.sh
+++ ubuntu-java/start_docker.sh
@@ -1,6 +1,9 @@
 # remove the rest containers if have
 docker compose down
 
+# Đảm bảo xóa container cũ nếu lệnh down chưa xử lý hết hoặc container được tạo thủ công
+docker rm -f ubuntu_java 2>/dev/null || true
+
 # build images
 docker compose build --no-cache
 
