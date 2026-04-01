a609867d83b062c30a349cb58aefaa229db62b97
Your Name <you@example.com>
2026-04-02 00:24:44 +0700

    Tét

--- ubuntu-python_point/start_docker.sh
+++ ubuntu-python_point/start_docker.sh
@@ -5,10 +5,8 @@ docker compose down
 
 # Tự động build lại image (nếu có thay đổi Dockerfile/requirements) có sử dụng cache cho nhanh
 # và chạy container ở chế độ nền (Detached mode)
-docker compose up -d --build
+docker compose up   --build
 
 echo "===================================================="
 echo "🚀 Container đã chạy ngầm thành công!"
-echo "👉 Để xem log backend, dùng lệnh: docker compose logs -f"
-echo "👉 Để chạy frontend, dùng lệnh: docker exec -it telua_python_point npm run dev"
 echo "===================================================="
