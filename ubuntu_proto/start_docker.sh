bbbf65d5b7eddd231d2d08e28fbf1a42f1c0225b
Thong Le Trung <thong.letrung@vn.bosch.com>
2026-06-18 17:15:40 +0700

    x

---
+++ ubuntu_proto/start_docker.sh
@@ -0,0 +1,10 @@
+# remove the rest containers if have
+docker compose down
+
+# build images
+docker compose build --no-cache
+
+# start up containers from all images
+docker compose up
+
+#docker compose up -d  ( -d Detached)
